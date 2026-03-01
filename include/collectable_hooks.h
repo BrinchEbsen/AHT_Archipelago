d25a894841580e0037a09e1e91947276b178e36d
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-03-01 21:01:27 +0100

    Added binary search function for finding the collectable

--- include/collectable_hooks.h
+++ include/collectable_hooks.h
@@ -11,4 +11,8 @@ void DragonEgg__Collected_VtableHook(void* this, Bool Register);
 void DarkXtal__Collected(void* this, Bool Register);
 void DarkXtal__Collected_VtableHook(void* this, Bool Register);
 
+void register_collectable(void* special_item, APCollectable_Type type);
+
+int binary_search_for_collectable(u16 map_index, u16 trigger_index, APCollectable_Type type);
+
 #endif /* COLLECTABLE_HOOKS_H */
--- src/collectable_hooks.c
+++ src/collectable_hooks.c
@@ -1,4 +1,5 @@
 #include <collectable_hooks.h>
+#include <ap_collectables.h>
 
 void LightXtal__Collected_VtableHook(void *this, Bool Register)
 {
@@ -13,4 +14,35 @@ void DragonEgg__Collected_VtableHook(void *this, Bool Register)
 void DarkXtal__Collected_VtableHook(void *this, Bool Register)
 {
     DarkXtal__Collected(this, Register);
+}
+
+void register_collectable(void* special_item, APCollectable_Type type)
+{
+
+}
+
+int binary_search_for_collectable(u16 map_index, u16 trigger_index)
+{
+    int start = AP_GRABBABLE_START;
+    int end = AP_GRABBABLE_START + AP_GRABBABLE_NUM - 1;
+
+    while (start <= end) {
+        int index = start + ((end-start)/2);
+        APCollectable* curr = &g_ap_collectables[index];
+
+        if ((map_index == curr->grabbable.map_index) &&
+            (trigger_index == curr->grabbable.trigger_index)) {
+            return index;
+        }
+
+        if ((map_index < curr->grabbable.map_index) ||
+            ((map_index == curr->grabbable.map_index) &&
+            (trigger_index < curr->grabbable.trigger_index))) {
+            end = index - 1;
+        } else {
+            start = index + 1;
+        }
+    }
+
+    return -1;
 }
