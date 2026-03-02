e14f69eca4b05cb70887484369a095acabf43a14
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-03-02 20:53:16 +0100

    Hook to disable bosses giving breath abilities (not implemented yet)

---
+++ asm/GC_NTSC/multi_BossDisableAbilityGain.s
@@ -0,0 +1,11 @@
+.memaddr 0x80031a0c
+# Disable Gnasty Gnorc enabling Electric Breath ability
+nop
+
+.memaddr 0x80038060
+# Disable Ineptune enabling Water Breath ability
+nop
+
+.memaddr 0x80044c3c
+# Disable Red enabling Ice Breath ability
+nop
