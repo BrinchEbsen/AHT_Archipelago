41acc6adc6c0a6b3829eb3492e32c2ce9484e775
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-03-06 00:42:40 +0100

    Line order in multi_GlideHooks.s was wrong

--- asm/GC_NTSC/multi_GlideHooks.s
+++ asm/GC_NTSC/multi_GlideHooks.s
@@ -1,20 +1,20 @@
-# Glide button check in HandleEndJump.
 .memaddr 0x800a7b9c
+# Glide button check in HandleEndJump.
 bl glide_XSEItemHandler_Player__ButtonDown_PreCallHook
 
-# Glide button check in HandleFall.
 .memaddr 0x800a29ec
+# Glide button check in HandleFall.
 bl glide_XSEItemHandler_Player__ButtonDown_PreCallHook
 
-# Glide button check in TestGlideStart.
 .memaddr 0x800a3c98
+# Glide button check in TestGlideStart.
 bl glide_XSEItemHandler_Player__ButtonDown_PreCallHook
 
-# Jump button check in TestJumpJumpGlide.
 .memaddr 0x800a6b7c
+# Jump button check in TestJumpJumpGlide.
 bl glide_XSEItemHandler_Player__ButtonDown_PreCallHook
 
+.memaddr 0x800a3ce4
 # Glide button check in TestGlideEnd.
 # This isn't strictly necessary but will ensure glide will end if it somehow starts.
-.memaddr 0x800a3ce4
 bl glide_XSEItemHandler_Player__ButtonDown_PreCallHook
