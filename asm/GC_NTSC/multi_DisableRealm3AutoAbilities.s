f82209a59e1a5845b6436db68d964ca2fb946a05
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-03-08 22:08:38 +0100

    Disable automatically obtained abilities in realm 3 levels

---
+++ asm/GC_NTSC/multi_DisableRealm3AutoAbilities.s
@@ -0,0 +1,11 @@
+.memaddr 0x801c7330
+# Map_FrostbiteVillage::v_OnRunning
+nop
+
+.memaddr 0x801ccb24
+# Map_IceCitadel::v_OnRunning
+nop
+
+.memaddr 0x801d3340
+# Map_MountainPass::v_OnRunning
+nop
