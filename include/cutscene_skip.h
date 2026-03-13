802396e77477d14ad440fa36bd41295dfd1a80e2
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-03-13 16:31:20 +0100

    Added exception system to cutscene skips, because i found a cutscene that crashes the game when skipped

--- include/cutscene_skip.h
+++ include/cutscene_skip.h
@@ -6,7 +6,17 @@
 #define GET_MAP_LOADING (gMapList.m_List[0].m_pMap)
 #define GET_MAP_TITLES  (gMapList.m_List[56].m_pMap)
 
-#define XITEMHANDLER_CAMERA_CUTSCENE_M_PSCRIPT(self) OFFSET_VAL(void*, self, 0x434)
+#define XITEMHANDLER_CUTSCENE_M_FILE_HASH(self) OFFSET_VAL(EXHashCode, self, 0x428)
+#define XITEMHANDLER_CUTSCENE_M_SCENE_HASH(self) OFFSET_VAL(EXHashCode, self, 0x42c)
+#define XITEMHANDLER_CUTSCENE_M_PSCRIPT(self) OFFSET_VAL(void*, self, 0x434)
+
+typedef struct FileSceneHash
+{
+    EXHashCode file_hash;
+    EXHashCode scene_hash;
+} FileSceneHash;
+
+extern FileSceneHash cutscenes_to_ignore[];
 
 extern int XItemHandler_Cutscene__Update(void* self);
 extern void EXItemAnimator_Script__SetScriptStatus(void* self, u32 Status);
--- src/cutscene_skip.c
+++ src/cutscene_skip.c
@@ -3,17 +3,38 @@
 #include <pad.h>
 #include <player.h>
 #include <ap_settings.h>
+#include <hashcodes.h>
+
+// Cutscenes that crash/cause issues and thus should not be skipped
+FileSceneHash cutscenes_to_ignore[] = {
+    {
+        .file_hash = HT_File_C47_R3C,
+        .scene_hash = HT_Script_CutsceneF
+    }
+};
+#define NUM_CUTSCENES_TO_IGNORE 1
 
 int XItemHandler_Cutscene__Update_VtableHook(void *self)
 {
     if (g_gamestate_ap_settings.skip_cutscene_button && g_pad_button_edge_down(PAD_BUTTON_Y)) {
-        // We don't want to skip the "cutscene" in the
-        // title or loading screen, as this causes a crash.
-        m_States loading_state = GET_MAP_LOADING->m_State;
-        m_States titles_state = GET_MAP_TITLES->m_State;
-        
-        if ((loading_state != Running) && (titles_state != Running)) {
-            skip_cutscene(self);
+        bool ignore = false;
+        for (int i = 0; i < NUM_CUTSCENES_TO_IGNORE; i++) {
+            if ((XITEMHANDLER_CUTSCENE_M_FILE_HASH(self) == cutscenes_to_ignore[i].file_hash) &&
+                (XITEMHANDLER_CUTSCENE_M_SCENE_HASH(self) == cutscenes_to_ignore[i].scene_hash)) {
+                ignore = true;
+                break;
+            }
+        }
+
+        if (!ignore) {
+            // We don't want to skip the "cutscene" in the
+            // title or loading screen, as this causes a crash.
+            m_States loading_state = GET_MAP_LOADING->m_State;
+            m_States titles_state = GET_MAP_TITLES->m_State;
+            
+            if ((loading_state != Running) && (titles_state != Running)) {
+                skip_cutscene(self);
+            }
         }
     }
 
@@ -28,7 +49,7 @@ void skip_cutscene(void* p_cutscene)
     }
 
     if (controls_locked) {
-        void* script = XITEMHANDLER_CAMERA_CUTSCENE_M_PSCRIPT(p_cutscene);
+        void* script = XITEMHANDLER_CUTSCENE_M_PSCRIPT(p_cutscene);
         EXItemAnimator_Script__SetScriptStatus(script, 3);
     }
 }
