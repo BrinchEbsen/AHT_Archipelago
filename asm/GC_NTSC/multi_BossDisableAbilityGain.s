40fa89234e84ec06fdece927134205c005fa26ee
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-03-18 22:51:05 +0100

    Actually enabled the patch to disable bosses giving the player breath abilities

--- asm/GC_NTSC/multi_BossDisableAbilityGain.s
+++ asm/GC_NTSC/multi_BossDisableAbilityGain.s
@@ -2,10 +2,24 @@
 # Disable Gnasty Gnorc enabling Electric Breath ability
 nop
 
+.memaddr 0x80031a0c
+# Disable Gnasty Gnorc switching player to Electric Breath
+nop
+
 .memaddr 0x80038060
 # Disable Ineptune enabling Water Breath ability
+nop
+nop
+
+.memaddr 0x800388dc
+# Disable Ineptune switching player to Water Breath
 nop
 
 .memaddr 0x80044c3c
 # Disable Red enabling Ice Breath ability
+nop
+nop
+
+.memaddr 0x80044c50
+# Disable Red switching player to Ice Breath
 nop
