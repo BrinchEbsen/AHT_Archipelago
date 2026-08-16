9a6ee201ab84ff491ed2a638dfbc6f1ba3aad16d
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-08-16 03:22:05 +0200

    Nop'd out maps setting their "Selected" minimap status when you enter them.
    This is to strictly only enable realm teleporter access when the client sets the flag for it in the ap settings.

---
+++ asm/GC_NTSC/disable_maps_setting_selectable_bit.s
@@ -0,0 +1,4 @@
+# Disable maps setting the "Selectable" bit for their minimap when loaded.
+# This is to prevent a realm from being accessible with the realm teleporter
+# before the client sets the access for it.
+nop
