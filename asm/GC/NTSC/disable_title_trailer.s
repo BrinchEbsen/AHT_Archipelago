89cc8179ee3846e06dfa292159aab93eb921e1f2
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-09-11 20:55:51 +0200

    The hook `disable_title_trailer` is NTSC-only and so is put in the NTSC folder.

--- asm/GC/NTSC/disable_title_trailer.s
+++ asm/GC/NTSC/disable_title_trailer.s
@@ -1,5 +1,7 @@
 # Disables the trailer movie playing on the title screen
 # when left idle for 30 seconds.
+#
+# NTSC only; PAL doesn't play anything on the title screen.
 # 
 # Normally this compares the idle pad timer to 30 seconds.
 # This compares 30 seconds to 30 seconds, so the 'ble-'
